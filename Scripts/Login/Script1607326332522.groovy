9fe7d2bb47da0c5b137d223a78ef186503af0b5e
yan-ardiansyah <yan.ardiansyah90@gmail.com>
2020-12-08 14:12:45 +0700

    add Screenshot function

--- Scripts/Login/Script1607326332522.groovy
+++ Scripts/Login/Script1607326332522.groovy
@@ -23,6 +23,12 @@ WebUI.comment(testcase)
 'Launch the Web'
 WebUI.openBrowser(GlobalVariable.url)
 
+today = new Date()
+String todaysDate = today.format('MM_dd_yy')
+String nowTime = today.format('hh_mm_ss')
+
+WebUI.takeScreenshot(GlobalVariable.ss + todaysDate + nowTime + ".png")
+
 'Wait until Element Home visible'
 WebUI.waitForElementVisible(findTestObject('Login/Page_STORE/Home'), 9)
 
@@ -43,6 +49,8 @@ WebUI.waitForElementVisible(findTestObject('Login/Page_STORE/input_Username'), 9
 
 'Verify element field input username'
 WebUI.verifyElementVisible(findTestObject('Login/Page_STORE/input_Username'))
+
+WebUI.takeScreenshot(GlobalVariable.ss + testcase + todaysDate + nowTime + ".png")
 
 'Set username on the field'
 WebUI.setText(findTestObject('Login/Page_STORE/input_Username'), username)
@@ -66,25 +74,35 @@ if (WebUI.verifyElementVisible(findTestObject('Login/Page_STORE/nama user'), Fai
     'Get Name of User'
     name_user = WebUI.getText(findTestObject('Login/Page_STORE/nama user'))
 
+    'Print nama_user'
     println(name_user)
 
+    'Verify nama_user equal to username var from Data Files'
     WebUI.verifyEqual(name_user, 'Welcome ' + username)
+
+    WebUI.takeScreenshot(GlobalVariable.ss + name_user + todaysDate + nowTime + ".png")
 } else {
     'Click Login button'
     WebUI.click(findTestObject('Login/Page_STORE/button_Log in'))
 
+    'Wait for alert appear'
     WebUI.waitForAlert(0)
 
+    'Verify alert present'
     WebUI.verifyAlertPresent(0)
 
+    'Call factory driver function'
     WebDriver driver = DriverFactory.getWebDriver()
 
+    'Get alert text and save it to alert var'
     alert = driver.switchTo().alert().getText()
 
+	WebUI.takeScreenshot(GlobalVariable.ss + alert + todaysDate + nowTime + ".png")
+	
+	'Print alert var'
     println(alert)
 
+    'Verify alert var value equal to alert_m var from Data Files'
     WebUI.verifyEqual(alert, alert_m)
-
-    WebUI.acceptAlert()
 }
 
